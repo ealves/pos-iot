b3bfa1dd96605175eb06da5f46ca3fd6fa07d8da
unknown <eduardoalves.info@gmail.com>
2022-06-09 23:18:02 -0300

    Adicionando mudança de estilo

--- Atividade1/app/src/main/java/com/example/atividade1/MainActivity.java
+++ Atividade1/app/src/main/java/com/example/atividade1/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.atividade1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -15,12 +17,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private String actualText = "";
     private int actualColor = R.color.black;
 
-    private EditText editText;
+    private boolean isBold = false;
+    private boolean isItalic = false;
+    private boolean isUpperCase = false;
+
     private TextView textView;
+    private EditText editText;
     private TextView textView5;
     private Button btnSendText;
-
     private SeekBar seekBar;
+
+    private CheckBox checkBox;
+    private CheckBox checkBox2;
+    private CheckBox checkBox3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +39,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         editText = findViewById(R.id.editText);
         btnSendText = findViewById(R.id.imageButton);
         textView = findViewById(R.id.textView);
+        textView5 = findViewById(R.id.textView5);
+        seekBar = findViewById(R.id.seekBar);
 
-        seekBar.findViewById(R.id.seekBar);
+        checkBox = findViewById(R.id.checkBox);
+        checkBox2 = findViewById(R.id.checkBox2);
+        checkBox3 = findViewById(R.id.checkBox3);
 
         btnSendText.setOnClickListener(this);
         seekBar.setOnSeekBarChangeListener(this);
@@ -39,8 +52,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        if (view.getId() == R.id.imageButton){
+        if (view.getId() == R.id.imageButton) {
 
+            actualText = editText.getText().toString();
             textView.setText(editText.getText().toString());
 
         }
@@ -52,6 +66,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             actualFontSize = progress;
             textView.setTextSize(actualFontSize);
 
+            textView5.setText(Integer.toString(actualFontSize) + "sp");
         }
     }
 
@@ -63,5 +78,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
 
+    }
+
+
+    public void onCheckboxClicked(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.checkBox:
+                isBold = checked;
+                break;
+            case R.id.checkBox2:
+                isItalic = checked;
+                break;
+            case R.id.checkBox3:
+                if (checked) {
+                    actualText = actualText.toUpperCase();
+                } else {
+                    actualText = actualText.toLowerCase();
+                }
+
+                textView.setText(actualText);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + view.getId());
+        }
+
+        if (isBold && !isItalic) {
+            textView.setTypeface(null, Typeface.BOLD);
+        }
+        else if (isBold && isItalic) {
+            textView.setTypeface(null, Typeface.BOLD_ITALIC);
+        }
+        else if (!isBold && !isItalic){
+            textView.setTypeface(null, Typeface.NORMAL);
+        }
+        else {
+            textView.setTypeface(null, Typeface.ITALIC);
+        }
     }
 }
